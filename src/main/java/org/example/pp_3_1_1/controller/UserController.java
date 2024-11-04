2944c6f07bee492823caa9e4914f9fb83ddde24c
aylenleen <aylenleen3695@gmail.com>
2024-11-04 12:37:23 +0300

    исправлено

--- src/main/java/org/example/pp_3_1_1/controller/UserController.java
+++ src/main/java/org/example/pp_3_1_1/controller/UserController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @EnableAutoConfiguration
@@ -22,32 +19,32 @@ public class UserController {
         this.userService = userService;
     }
 
-    @RequestMapping(value ="/", method = RequestMethod.GET)
+    @GetMapping(value ="/")
     public String showAllUsers(Model model) {
         model.addAttribute("users", userService.getAllUsers());
         return "index";
     }
 
-    @RequestMapping(value ="/addUser", method = RequestMethod.GET)
+    @GetMapping(value ="/addUser")
     public String addUser(Model model) {
         User user = new User();
         model.addAttribute("user", user);
         return "user-info";
     }
 
-    @RequestMapping(value = "/saveUser", method = RequestMethod.POST)
+    @PostMapping(value = "/saveUser")
     public String saveUser(@ModelAttribute("user") User user) {
         userService.add(user);
         return "redirect:/";
     }
 
-    @RequestMapping(value = "/updateUser", method = RequestMethod.POST)
+    @PostMapping(value = "/updateUser")
     public String updateUser(@ModelAttribute("user") User user) {
         userService.update(user);
         return "redirect:/";
     }
 
-    @RequestMapping(value ="/updateInfo", method = RequestMethod.GET)
+    @GetMapping(value ="/updateInfo")
     public String updateUser(@RequestParam("userId") int id, Model model) {
         model.addAttribute("action", "update");
         User user = userService.getUserById(id);
@@ -55,7 +52,7 @@ public class UserController {
         return "user-info";
     }
 
-    @RequestMapping(value ="/deleteUser", method = RequestMethod.GET)
+    @GetMapping(value ="/deleteUser")
     public String deleteUser(@RequestParam("userId") int id) {
         User user = userService.getUserById(id);
         userService.delete(user);
